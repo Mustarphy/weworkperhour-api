b29b4efb1b8426f8817d0073d277f1a36a449622
Mustarphy <abdulazeezmustapha204@gmail.com>
2025-11-26 23:58:31 -0800

    added approved email methods

---
+++ resources/views/emails/applicant-rejected.blade.php
@@ -0,0 +1,8 @@
+<h2>Hello {{ $user->name }},</h2>
+
+<p>Your application for <strong>{{ $job->title }}</strong> has been <strong>rejected</strong>.</p>
+
+<p>Our team will contact you soon.</p>
+
+<p>Thank you,<br>
+{{ $job->company->name }}</p>
